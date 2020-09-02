bb2b5e311da58141009ca2d543ef30849e727aa3
pat-s <patrick.schratz@gmail.com>
2020-09-02 17:34:56 +0200

    add rstudio daily linux installer

--- R/update-rstudio-daily.R
+++ R/update-rstudio-daily.R
@@ -1,7 +1,9 @@
 update_rstudio_daily <- function() {
   switch (Sys.info()[["sysname"]],
     "Linux" = {
-      system2("wget https://rstudio.org/download/latest/daily/desktop/bionic/rstudio-latest-amd64.deb")
+      system2("wget", c("https://rstudio.org/download/latest/daily/desktop/bionic/rstudio-latest-amd64.deb"))
+      system2("sudo", c("dpkg", "-i", "rstudio-latest-amd64.deb"))
+      system2("rm", c("rstudio-latest-amd64.deb"))
     },
     "Darwin" = {
       system2("brew", c("cask", "reinstall", "rstudio-daily"))
