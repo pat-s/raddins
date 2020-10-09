695cad9ddd8b818cc3b0baaf6a93470c342f8118
pat-s <patrick.schratz@gmail.com>
2020-10-09 22:05:03 +0200

    update_rstudio() -> install_rstudio()

--- R/update-rstudio-daily.R
+++ R/update-rstudio-daily.R
@@ -1,12 +1,13 @@
 #' @title Install RStudio
 #' @description Installs the latest RStudio daily version based on the operating system
 #' @export
-update_rstudio_daily <- function() {
+install_rstudio_daily <- function() {
   switch(Sys.info()[["sysname"]],
     "Linux" = {
       system2("wget", c("https://rstudio.org/download/latest/daily/desktop/bionic/rstudio-latest-amd64.deb"))
-      system2("sudo", c("dpkg", "-i", "rstudio-latest-amd64.deb"))
+      system("sudo -kS dpkg -i rstudio-latest-amd64.deb", input = rstudioapi::askForPassword("sudo password"))
       system2("rm", c("rstudio-latest-amd64.deb"))
+      rstudioapi::openProject()
     },
     "Darwin" = {
       system2("brew", c("cask", "reinstall", "rstudio-daily"))
